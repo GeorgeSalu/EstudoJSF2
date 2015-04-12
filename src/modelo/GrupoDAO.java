f0cf4473f748384f87b734f4c8b796d593f0d5da
George salu <george.salu10@gmail.com.br>
2015-04-11 22:24:43 -0300

    implementação de logica de paginação

--- src/modelo/GrupoDAO.java
+++ src/modelo/GrupoDAO.java
@@ -12,63 +12,165 @@ import jpa.EntityManagerUtil;
 public class GrupoDAO {
 
 	private EntityManager em;
+	private String ordem = "id";
+	private String filtro = "";
+	private Integer maximosObjetos = 2;
+	private Integer posicaoAtual = 0;
+	private Integer totalObjetos = 0;
 
 	public GrupoDAO() {
 		em = EntityManagerUtil.getEntityManager();
 	}
-	
-	public List<Grupo> listarTodos(){
-		return em.createQuery("from Grupo order by nome").getResultList();
+
+	public List<Grupo> listar(){
+		String where = "";
+		if(filtro.length() > 0){
+			if(ordem.equals("id")){
+				try{
+					Integer.parseInt(filtro);
+					where = " where "+ordem+" = '"+filtro+"'";
+				}catch(Exception e){
+					
+				}
+			}else{
+				where = " where upper("+ordem+") like '"+filtro.toUpperCase()+"%' ";
+			}
+		}
+		String jpql = "from Grupo "+where+" order by "+ordem;
+		totalObjetos = em.createQuery("select id from Grupo "+where+" order by "+ordem).getResultList().size();
+		return em.createQuery(jpql).setFirstResult(posicaoAtual).setMaxResults(maximosObjetos).getResultList();
 	}
 	
-	public boolean gravar(Grupo obj){
-		try{
+	public void primeiro(){
+		posicaoAtual = 0;
+	}
+	
+	public void anterior(){
+		posicaoAtual -= maximosObjetos;
+		if(posicaoAtual < 0){
+			posicaoAtual = 0;
+		}
+	}
+	
+	public void proximo(){
+		if(posicaoAtual + maximosObjetos < totalObjetos){
+			posicaoAtual += maximosObjetos;
+		}
+	}
+	
+	public void ultimo(){
+		int resto = totalObjetos % maximosObjetos;
+		
+		if(resto > 0){
+			posicaoAtual = totalObjetos - resto;
+		}else{
+			posicaoAtual = totalObjetos - maximosObjetos;
+		}
+	}
+	
+	public String getMensagemNavegacao(){
+		int ate = posicaoAtual + maximosObjetos;
+		if(ate > totalObjetos){
+			ate = totalObjetos;
+		}
+		
+		return "Listando de "+(posicaoAtual+1)+" ate "+ate+" de "+totalObjetos+" registros";
+	}
+	
+	public List<Grupo> listarTodos() {
+		return em.createQuery("from Grupo order by nome").getResultList();
+	}
+
+	public boolean gravar(Grupo obj) {
+		try {
 			em.getTransaction().begin();
-			if(obj.getId() == null){
+			if (obj.getId() == null) {
 				em.persist(obj);
-			}else{
+			} else {
 				em.merge(obj);
 			}
 			em.getTransaction().commit();
 			UtilMensagens.mensagensInformacao("Objeto Persistido com sucesso");
 			return true;
-		}catch(Exception ex){
-			if(em.getTransaction().isActive() == false){
+		} catch (Exception ex) {
+			if (em.getTransaction().isActive() == false) {
 				em.getTransaction().begin();
 			}
 			em.getTransaction().rollback();
-			UtilMensagens.mensagensErro("Erro ao persistir objeto: "+UtilErros.getMensagemErro(ex));
+			UtilMensagens.mensagensErro("Erro ao persistir objeto: "
+					+ UtilErros.getMensagemErro(ex));
 			return false;
 		}
 	}
-	
-	public boolean excluir(Grupo obj){
-		try{
+
+	public boolean excluir(Grupo obj) {
+		try {
 			em.getTransaction().begin();
 			em.remove(obj);
 			em.getTransaction().commit();
 			UtilMensagens.mensagensInformacao("Objeto removido com sucesso");
 			return true;
-		}catch(Exception ex){
-			if(em.getTransaction().isActive() == false){
+		} catch (Exception ex) {
+			if (em.getTransaction().isActive() == false) {
 				em.getTransaction().begin();
 			}
 			em.getTransaction().rollback();
-			UtilMensagens.mensagensErro("Erro ao remover objeto: "+UtilErros.getMensagemErro(ex));
+			UtilMensagens.mensagensErro("Erro ao remover objeto: "
+					+ UtilErros.getMensagemErro(ex));
 			return false;
 		}
 	}
-	
-	public Grupo localizar(Integer id){
+
+	public Grupo localizar(Integer id) {
 		return em.find(Grupo.class, id);
 	}
-	
+
 	public EntityManager getEm() {
 		return em;
 	}
 
 	public void setEm(EntityManager em) {
 		this.em = em;
+	}
+
+	public String getOrdem() {
+		return ordem;
+	}
+
+	public void setOrdem(String ordem) {
+		this.ordem = ordem;
+	}
+
+	public String getFiltro() {
+		return filtro;
+	}
+
+	public void setFiltro(String filtro) {
+		this.filtro = filtro;
+	}
+
+	public Integer getMaximosObjetos() {
+		return maximosObjetos;
+	}
+
+	public void setMaximosObjetos(Integer maximosObjetos) {
+		this.maximosObjetos = maximosObjetos;
+	}
+
+	public Integer getPosicaoAtual() {
+		return posicaoAtual;
+	}
+
+	public void setPosicaoAtual(Integer posicaoAtual) {
+		this.posicaoAtual = posicaoAtual;
+	}
+
+	public Integer getTotalObjetos() {
+		return totalObjetos;
+	}
+
+	public void setTotalObjetos(Integer totalObjetos) {
+		this.totalObjetos = totalObjetos;
 	}
 
 }
